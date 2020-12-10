fe6f60a4813d6315aabf58a184b3fd6812f4c15c
Gianluca Zanzarella <gianlucazanza@hotmail.com>
2020-12-10 12:13:28 +0100

    impostati elementi delle sezioni 5, 6, footer

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@
 const vm = new Vue ({
   el: '#root',
   data: {
-    headerNavLinks: ['Home', 'About us', 'Features', 'Testimonials', 'Contact US']
+    headerNavLinks: ['Home', 'About us', 'Features', 'Testimonials', 'Contact US'],
+    footerMenuLinks: ['About Us', 'Theme', 'Features', 'Pricing', 'Blog'],
+    footerAboutLinks: ['About Us', 'Theme', 'Features', 'Pricing', 'Blog'],
+    footerQuickLinks: ['About Us', 'Theme', 'Features', 'Pricing', 'Blog'],
   },
   methods: {
 
